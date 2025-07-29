c63ddd022c24bef79cad78fdd5df3ad09cec929f
ssset <claimov@gmail.com>
2025-07-29 23:14:31 +0300

    initial websockets + kafka recovery

--- app/logic/mediator/base.py
+++ app/logic/mediator/base.py
@@ -42,9 +42,6 @@ class Mediator(EventMediator,CommandMediator, QueryMediator):
 
         for event in events:
             handlers: Iterable[EventHandler] = self.events_map[event.__class__]
-            
-            for handler in handlers:
-                result.append(await handler.handle(event=event))
 
             result.extend([await handler.handle(event) for handler in handlers])
     
--- app/settings/config.py
+++ app/settings/config.py
@@ -9,6 +9,7 @@ class Config(BaseSettings):
     mongo_db_messages_collection: str = Field(default='messages', alias='MONGO_DB_MESSAGES_COLLECTION')
     new_chats_event_topic: str = Field(default='new_chats_topic')
     kafka_url: str = Field(alias='KAFKA_URL')
+    new_message_received_topic: str = Field(default='new_message_topic')
 
     model_config = SettingsConfigDict(
         env_file='.env',
